d6bbdd2690ec8217064a7bba7b8cb770662ffe41
LeThang34 <104156515+LeThang34@users.noreply.github.com>
2022-06-11 16:00:21 +0700

    update

--- js/jas.js
+++ js/jas.js
@@ -24,7 +24,7 @@ let lst_pk = document.querySelector('.list_pk')
 btnMoblie.addEventListener('click', function () {
     lst_mb.classList.add('active')
     lst_tb.classList.remove('active')
-    lst_w.classList.remove('active')    
+    lst_w.classList.remove('active')
     lst_hp.classList.remove('active')
     lst_pk.classList.remove('active')
 
@@ -34,7 +34,7 @@ btnMoblie.addEventListener('click', function () {
 btnTablet.addEventListener('click', function () {
     lst_mb.classList.remove('active')
     lst_tb.classList.add('active')
-    lst_w.classList.remove('active')    
+    lst_w.classList.remove('active')
     lst_hp.classList.remove('active')
     lst_pk.classList.remove('active')
 });
@@ -48,7 +48,7 @@ btnW.addEventListener('click', function () {
 btnHP.addEventListener('click', function () {
     lst_mb.classList.remove('active')
     lst_tb.classList.remove('active')
-    lst_w.classList.remove('active')    
+    lst_w.classList.remove('active')
     lst_hp.classList.add('active')
     lst_pk.classList.remove('active')
 });
@@ -56,9 +56,9 @@ btnPK.addEventListener('click', function () {
     lst_pk.classList.add('active')
     lst_mb.classList.remove('active')
     lst_tb.classList.remove('active')
-    lst_w.classList.remove('active')    
+    lst_w.classList.remove('active')
     lst_hp.classList.remove('active')
-   
+
 });
 
 
@@ -70,48 +70,77 @@ let thongTin3 = document.querySelector('.tt3')
 let listNew1 = document.querySelector('.lst_rp1')
 let listNew2 = document.querySelector('.lst_rp2')
 let listNew3 = document.querySelector('.lst_rp3')
+
+thongTin1.addEventListener('click', function () {
+    if (listNew1.style.display === 'none') {
+        listNew1.style.display = 'block';
+    }
+    else {
+        listNew1.style.display = 'none';
+    }
+})
+
+thongTin2.addEventListener('click', function () {
+    if (listNew2.style.display === 'none') {
+        listNew2.style.display = 'block';
+    }
+    else {
+        listNew2.style.display = 'none';
+    }
+})
+
+thongTin3.addEventListener('click', function () {
+    if (listNew3.style.display === 'none') {
+        listNew3.style.display = 'block';
+    }
+    else {
+        listNew3.style.display = 'none';
+    }
+})
+
+
+//----content----
+
+// sub slider 1 PC, Tablet
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+}
+// End sub slider1 PC,Tablet
+
+// Begin sub slider 1 mobile
+// End sub slider 1 mobile
+
+// slick js
+
+   
+
+//---End content---
+
+
 /*
-thongTin1.addEventListener('click', function(){
-    listNew1.classList.add('active')
-    listNew2.classList.remove('active')
-    listNew3.classList.remove('active')
-})
 
-thongTin2.addEventListener('click', function(){
-    listNew2.classList.add('active')
-    listNew1.classList.remove('active')
-    listNew3.classList.remove('active')
-})
 
-thongTin3.addEventListener('click', function(){
-    listNew3.classList.add('active')
-    listNew2.classList.remove('active')
-    listNew1.classList.remove('active')
-})*/
-
-thongTin1.addEventListener('click', function(){
-    if(listNew1.style.display === 'none'){
-        listNew1.style.display ='block';
-    }
-    else {
-        listNew1.style.display ='none';
-    }
-})
-
-thongTin2.addEventListener('click', function(){
-    if(listNew2.style.display === 'none'){
-        listNew2.style.display ='block';
-    }
-    else {
-        listNew2.style.display ='none';
-    }
-})
-
-thongTin3.addEventListener('click', function(){
-    if(listNew3.style.display === 'none'){
-        listNew3.style.display ='block';
-    }
-    else {
-        listNew3.style.display ='none';
-    }
-})
+*/
